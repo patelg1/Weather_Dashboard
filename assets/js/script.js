4943536da19643877f5d82ca3f017fa7dcb23a76
Gopal Patel <patelgopalk1@gmail.com>
2021-02-03 23:05:21 -0500

    Fixed ajax call to get data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,65 @@
 var currentDate = dayjs().format("M/DD/YYYY");
+var storedCity = JSON.parse(localStorage.getItem("searchedCity")) || [];
 
 
 
 $(document).ready(function(){
     
     function getCityWeather(cityName){
-        var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=7c116c195b43008fb23ae55ea85959a7&units=imperial";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=7c116c195b43008fb23ae55ea85959a7";
         
         
         $.ajax({
-            url: queryURL,
-            dataType: "json",
+            url: queryURL,            
             method: "GET"
         }).then(function(response){
             console.log(response);
             $("#current-city").empty();
+
+            var searchedCity = $("<h3>").text(response.name);
+            var displayDate = searchedCity.append(" " + currentDate);
+            var temperature = $("<p>").addClass("the-temperature").text("Temperature: " + response.main.temp + "F");
+            var humidity = $("<p>").addClass("the-humidity").text("Humidity: " + response.main.humidity + "%");
+            var windSpeed = $("<p>").addClass("the-windspeed").text("Wind Speed: " + response.wind.speed + "MPH");
+            var weatherIcon = $("<img>").addClass("weather-image").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+
+            $("#current-city").append(searchedCity, displayDate, temperature, humidity, windSpeed, weatherIcon);
             
-        })
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?&appid=7c116c195b43008fb23ae55ea85959a7&lat=" + lat + "&lon=" + lon;
 
-        var searchedCity = $("<h3>").text(response.name);
-        var displayDate = searchedCity.append(" " + currentDate);
-        var temperature = $("<p>").addClass("the-temperature").text("Temperature: " + response.main.temp + "F");
-        var humidity = $("<p>").addClass("the-humidity").text("Humidity: " + response.main.humidity + "%");
-        var windSpeed = $("<p>").addClass("the-windspeed").text("Wind Speed: " + response.wind.speed + "MPH");
-        var weatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png");
+            $.ajax({
+                url: queryURL2,
+                method: "GET",
+            }).then(function (response) {
 
-        $("#current-city").append(searchedCity, displayDate, temperature, humidity, windSpeed, weatherIcon);
+                var uvI = response.value;
+                var uvIndex = $("<p>").addClass("uvIndex").text("UV Index: " + uvI); 
+                if (uvI < 3){
+                    uvIndex.addClass("lowUV");
+                }else if (uvI >= 3 && uvI <= 5){
+                    uvIndex.addClass("moderateUV");
+                }else{
+                    uvIndex.addClass("highUV");
+                }
+                $("#current-city").append(uvIndex);
 
-        var lat = response.city.coord.lat;
-        var lon = response.city.coord.lon;
-        var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?&appid=7c116c195b43008fb23ae55ea85959a7&lat=" + lat + "&lon=" + lon;
 
-        $.ajax({
-            url: queryURL2,
-            method: "GET",
-        }).then(function (response) {
-            var uvI = response.value;
-            $(".uvIndex").text("UV Index: " + uvI);
-        });
+            });
         
 
       // var uvIndex = $("<p>").addClass("the-uvIndex").text("UV Index: " + response.main.temp + "F")
         getFiveDay(cityName);
+        })
+
+        
+
+        
     }
 
-    function getFiveDay(city){
-        var fiveDayURL = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=7c116c195b43008fb23ae55ea85959a7&units=imperial";
+    function getFiveDay(cityName){
+        var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=7c116c195b43008fb23ae55ea85959a7&units=imperial";
 
 
         $.ajax({
@@ -56,7 +70,7 @@ $(document).ready(function(){
             console.log(response);
             $("#fiveday").empty();
 
-            for (var i = 0; i < response.list.length; i++){
+            for (var i = 1; i < response.list.length; i+=8){
                 var day = $("<h4>").text(new Date(response.list[i].dt_txt).toLocaleDateString())
                 var maxTemp = $("<p>").text("Temperature: " + response.list[i].main.temp_max + "F");
                 var humid = $("<p>").text("Humidity: " + response.list[i].main.humidity + "%")
@@ -69,11 +83,16 @@ $(document).ready(function(){
      
     $("#search-button").on("click", function(event){
         event.preventDefault();
+        var cityName = $("#city-search").val();
+        storedCity.push(cityName);
+        var cityArray = JSON.stringify(storedCity);
 
-        
+        localStorage.setItem("searchedCity", cityArray);
+
+        getCityWeather(cityName);
     })
 
-getCityWeather();
+
 
 
 })
